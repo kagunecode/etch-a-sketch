232fc240bad207b33b5a07a85da3af7bb2478086
kagunecode <cesar.acb98@gmail.com>
2023-07-26 18:18:57 -0500

    Fixed function logic for page events

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,46 @@
-function drawBoard(boardSize = 16, body) {
+function drawBoard(boardSize, body) {
+    console.log("DRAWING BOARD!")
+    let pixelDivider = 1;
+    if (boardSize == "32x32") {
+        pixelDivider = 2;
+        boardSize = 32;
+    } else if (boardSize == "64x64") {
+        pixelDivider = 4;
+        boardSize = 64;
+    } else {
+        pixelDivider = 1;
+        boardSize = 16;
+    }
     for (let j=0; j<boardSize; j++) {
         for (let i=0; i<boardSize; i++) {    
             const div = document.createElement('div');
-            div.style.width = '40px';
-            div.style.height = '40px';   
+            let pixelSize = 40 / pixelDivider;
+            div.style.width = `${pixelSize}px`;
+            div.style.height = `${pixelSize}px`;   
             div.classList.add('pixel')
             body.appendChild(div);
         }
     }
+}
+
+function redrawBoard () {
+    let canvasSize = drawContainer.childElementCount;
+    console.log("Redrawing board!")
+    for (let i=0; i<canvasSize; i++) {
+        drawContainer.removeChild(drawContainer.lastElementChild)
+    }
+}
+
+function colorPixel(pixels, color) {
+    pixels.forEach(pixel => pixel.addEventListener('mouseover', (e) => {
+        if (e.target.style.backgroundColor == "" || e.target.style.backgroundColor == "white") {
+            e.target.style.backgroundColor = color.value;
+        }
+    }))
+}
+
+function eraseBoard(pixels) {
+    pixels.forEach(pixel => pixel.style.backgroundColor = "white") 
 }
 
 const drawContainer = document.querySelector('.sketch');
@@ -16,10 +49,12 @@ drawBoard(16, drawContainer);
 const currentColor = document.querySelector('#colorpicker');
 const eraseAll = document.querySelector('.erase-all');
 const pixels = document.querySelectorAll('.pixel');
-pixels.forEach(pixel => pixel.addEventListener('mouseover', (e) => {
-    if (e.target.style.backgroundColor == "") {
-        console.log('im working')
-        e.target.style.backgroundColor = currentColor.value;
-    } else {
-    }
-}));
+const canvasSizeButton = document.querySelectorAll('.canvas-size-button');
+
+
+drawContainer.addEventListener('mouseover', colorPixel.bind(null, pixels, currentColor));
+eraseAll.addEventListener('click', eraseBoard.bind(null, pixels));
+canvasSizeButton.forEach(button => button.addEventListener('click', (e) => {
+    redrawBoard();
+}))
+
