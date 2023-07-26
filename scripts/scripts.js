53b48e6e7de251ff909b4557d32e4d72e996c253
kagunecode <cesar.acb98@gmail.com>
2023-07-26 00:39:37 -0500

    Added the pixel red painting when hovering

---
+++ scripts/scripts.js
@@ -0,0 +1,21 @@
+function drawBoard(boardSize = 16, body) {
+    for (let j=0; j<boardSize; j++) {
+        for (let i=0; i<boardSize; i++) {    
+            const div = document.createElement('div');
+            div.style.width = '30px';
+            div.style.height = '30px';   
+            div.classList.add('pixel')
+            body.appendChild(div);
+        }
+    }
+}
+
+const drawContainer = document.querySelector('.container');
+drawBoard(16, drawContainer);
+
+const pixels = document.querySelectorAll('.pixel');
+console.log(pixels)
+pixels.forEach(pixel => pixel.addEventListener('mouseover', (e) => {
+    console.log(e);
+    e.target.classList.add('pixel-red');
+}))
