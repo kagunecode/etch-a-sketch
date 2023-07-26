f1bb3bf3679d4565218327f1b97ce9861c03e235
kagunecode <cesar.acb98@gmail.com>
2023-07-26 02:03:20 -0500

    Removed erase function for debugging

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,15 +11,15 @@ function drawBoard(boardSize = 16, body) {
 }
 
 const drawContainer = document.querySelector('.sketch');
-const currentColor = document.querySelector('#colorpicker');
 drawBoard(16, drawContainer);
 
+const currentColor = document.querySelector('#colorpicker');
+const eraseAll = document.querySelector('.erase-all');
 const pixels = document.querySelectorAll('.pixel');
 pixels.forEach(pixel => pixel.addEventListener('mouseover', (e) => {
-    console.log(e.target.style.color == "")
     if (e.target.style.backgroundColor == "") {
-        console.log(currentColor.value)
+        console.log('im working')
         e.target.style.backgroundColor = currentColor.value;
     } else {
     }
-}))
+}));
